4a117aa1c7015f55e69b44e0d06db660e58817c5
Bang Nguyen <bangnv77@gmail.com>
2016-10-24 20:48:50 -0700

    install FFaker gem and create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do |n|
+  Traveler.create!(first_name: FFaker::Name.first_name,
+                    last_name: FFaker::Name.last_name,
+                    birthday: FFaker::IdentificationESCO.expedition_date,
+                    gender: FFaker::Gender.maybe )
+end
